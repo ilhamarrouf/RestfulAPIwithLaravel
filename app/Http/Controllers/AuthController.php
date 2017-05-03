3676b2414937bc7396101bb7db082af5e08678ac
ilhamarrouf <ilham.arrouf@gmail.com>
2017-05-03 10:30:16 +0700

    register user

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Transformers\UserTransformer;
+
+class AuthController extends Controller
+{
+    public function register(Request $request, User $user)
+    {
+    	$this->validate($request, [
+    		'name'		=> 'required',
+    		'email'		=> 'required|email|unique:users',
+    		'password'	=> 'required|min:6'
+    	]);
+
+    	$user = $user->create([
+    		'name'		=> $request->name,
+    		'email'		=> $request->email,
+    		'password'	=> bcrypt($request->password),
+    		'api_token'	=> bcrypt($request->email)
+    	]);
+
+    	$response = fractal()
+    		->item($user)
+    		->transformWith(new UserTransformer)
+    		->toArray();
+
+    	return response()->json($response, 201);
+    }
+}
